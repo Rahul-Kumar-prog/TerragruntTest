0b1824d6de90ee31aa9e2014e528c8c3f134c105
Rahul <rahul@Rahuls-Mac-mini.local>
2025-10-08 22:52:56 +0530

    Testing files

---
+++ old/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    assume_role = {
+      role_arn = "arn:aws:iam::123456789:role/role_name"
+    }
+    bucket                  = "some state bucket"
+    dynamodb_table          = "some lock table"
+    encrypt                 = true
+    key                     = "someplace/terraform.tfstate"
+    region                  = "us-east-1"
+    shared_credentials_file = "/tmp/state_credentials"
+  }
+}
